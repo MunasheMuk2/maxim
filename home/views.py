12ceb171232f4fdc8d9c26797918923dfdf54b66
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-25 20:35:26 +0100

    Add content to base and index.html

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def my_home(request):
-    return HttpResponse("Hello, World!")
+def home(request):
+    return render(request, "home.html")
