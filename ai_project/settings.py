7ac252724defdf891ec57ed81bfb2208ccdc0ad9
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-25 14:53:14 +0100

    Deployment settings

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)ky=+qog0bh&o9pqd#w+%ea$cwwgwf#%)yd4=*r=pn-o9b(0r="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
