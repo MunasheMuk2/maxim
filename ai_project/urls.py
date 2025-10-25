bb5a78fa3fbbf8acd1cce84673408b2357973efe
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-26 00:14:23 +0100

    Fix URL to use index view

--- ai_project/urls.py
+++ ai_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from home.views import home
+from home.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home, name="home"),
+    path("", index, name="home"),
 ]
