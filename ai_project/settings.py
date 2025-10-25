5d25bef1c18d1a73413fd368fc656bbd13051720
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-25 17:56:19 +0100

    Configure static files for Heroku

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -56,7 +56,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+# Static files
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "ai_project.urls"
 
