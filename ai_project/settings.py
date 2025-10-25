d58e590c553b4fba2add2e477c788ce2a2b036e0
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-25 15:23:55 +0100

    Add secret key

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)ky=+qog0bh&o9pqd#w+%ea$cwwgwf#%)yd4=*r=pn-o9b(0r="
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
