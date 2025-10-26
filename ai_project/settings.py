dfcb6d04c555d2fa5ebe77534dccb731629727e0
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-26 09:19:36 +0000

    Fix allauth login for Heroku

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -47,6 +47,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "home",
+    "allauth",
+    "allauth.account",
+    # Optional -- requires install using `django-allauth[socialaccount]`.
+    "allauth.socialaccount",
 ]
 
 MIDDLEWARE = [
@@ -76,13 +80,22 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
-                "django.template.context_processors.request",
+                "django.template.context_processors.request",  # required by alluth
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by email
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = "ai_project.wsgi.application"
 
