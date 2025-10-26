351ee15d5b040539dfadd2905c68763af6ee4f4b
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-26 09:19:36 +0000

    Fix allauth login for Heroku

--- ai_project/urls.py
+++ ai_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", index, name="home"),
 ]
