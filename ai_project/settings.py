a97cad67582e4ceb2c53b1ced8c4fe451445880c
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-26 09:28:28 +0000

    Add allauth AccountMiddleware

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 # Static files
