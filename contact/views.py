150691dfd1ea2cb61af9fd6b02037ac2710a2322
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-11-03 22:26:15 +0000

    Add contact form with forms.py

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import ContactForm
 
 # Create your views here.
 
 
+# def contact_view(request):
+#     return render(request, "contact/contact.html")
+
+
 def contact_view(request):
-    return render(request, "contact/contact.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  # saves to database
+            messages.success(request, "Message Received! Thank you.")
+            return redirect("contact")  # redirect after success
+    else:
+        form = ContactForm()
+    return render(request, "contact/contact.html", {"form": form})
