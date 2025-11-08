5ec81a81726fd76a68914587a4674baa0b438352
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-11-08 20:21:32 +0000

    Amend checkout urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "checkout"  # ✅ This defines the namespace!
+app_name = "checkout"
 
 urlpatterns = [
-    path("<str:plan>/", views.create_checkout_session, name="create_checkout_session"),
+    path("creator/", views.creator_checkout, name="creator_checkout"),
+    path("pro/", views.pro_checkout, name="pro_checkout"),
+    path("", views.checkout_page, name="checkout"),  # Optional default
     path("success/", views.checkout_success, name="checkout_success"),
 ]
