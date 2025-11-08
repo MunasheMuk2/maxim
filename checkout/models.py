d82bfe41df52b344a61184f3c63ff6077af1fda6
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-11-08 18:57:25 +0000

    Add subscription model to checkout app models.pty

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Subscription(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
+    stripe_subscription_id = models.CharField(max_length=255, blank=True, null=True)
+    plan = models.CharField(max_length=50, blank=True, null=True)
+    active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
