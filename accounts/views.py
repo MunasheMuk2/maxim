c21788c24ab23643446689719842d50e05bcd775
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-11-10 18:54:27 +0000

    Update views to handle form

--- accounts/views.py
+++ accounts/views.py
@@ -12,4 +12,32 @@ def checkout_page(request):
 
 @login_required
 def premium_content(request):
-    return render(request, "accounts/premium_dashboard.html")
+    user = request.user
+    profile = user.userprofile
+
+    # Block if out of requests
+    if profile.custom_requests_remaining < 1:
+        message = (
+            "You've used all your custom requests. Wait for next month or upgrade."
+        )
+        return render(
+            request,
+            "accounts/premium_dashboard.html",
+            {"form": None, "message": message, "profile": profile},
+        )
+
+    if request.method == "POST":
+        form = CustomRequestForm(request.POST, request.FILES)
+        if form.is_valid():
+            custom_request = form.save(commit=False)
+            custom_request.user = user
+            custom_request.save()
+            profile.custom_requests_remaining -= 1
+            profile.save()
+            return redirect("premium_content")
+    else:
+        form = CustomRequestForm()
+
+    return render(
+        request, "accounts/premium_dashboard.html", {"form": form, "profile": profile}
+    )
