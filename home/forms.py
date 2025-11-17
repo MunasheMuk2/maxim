d8c39988673a7c4b4a042e9ba5858a0bf5e3d509
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-11-17 00:05:14 +0000

    Add required to creator form on home page

--- home/forms.py
+++ home/forms.py
@@ -6,8 +6,3 @@ class CreatorApplicationForm(forms.ModelForm):
     class Meta:
         model = CreatorApplication
         fields = ["name", "email", "speciality", "message"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.required = True
